9477a2e296ad8d87fd2a02a28e403443b35c52af
Shridhar Giri <Shridhar.Giri@ivycomptech.com>
2023-08-15 16:14:47 +0530

    Commiting buildJob

---
+++ src/test/java/buildClasses/BuildClass1.java
@@ -0,0 +1,13 @@
+package buildClasses;
+
+import org.testng.annotations.Test;
+
+public class BuildClass1 {
+	
+	@Test
+	public void testPrint()
+	{
+		System.out.println("Completed developer classes execution for creating build.");
+	}
+
+}
---
+++ src/test/java/buildClasses/BuildClass2.java
@@ -0,0 +1,13 @@
+package buildClasses;
+
+import org.testng.annotations.Test;
+
+public class BuildClass2 {
+	
+	@Test
+	public void testPrint()
+	{
+		System.out.println("Build is ready please deploy it on any environment.");
+	}
+
+}
